c2c82feecc5d5a3a4536a2b8f0c241a8103681d1
GhostGooner <153914103+GhostGooner@users.noreply.github.com>
2024-04-10 10:53:58 +0300

    Update new_ext.sh

--- Lesson3-4/new_ext.sh
+++ Lesson3-4/new_ext.sh
@@ -2,6 +2,13 @@
 
 read -p "Напишите имя файла с расширением " filename
 
+if
+ [[ ! $filename == *.* ]]; then
+    echo "Расширение файла не указано"
+
+read -p "Напишите имя файла с расширением " filename
+fi
+
 read -p "Укажите новое расширение " new_extension
 
 filename_no_extension="${filename%.*}"
