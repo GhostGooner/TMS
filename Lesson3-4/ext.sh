2a5a5d832f0dfc75d40ee26c8f3e3a307809fbf5
GhostGooner <153914103+GhostGooner@users.noreply.github.com>
2024-04-10 11:17:49 +0300

    Update ext.sh

--- Lesson3-4/ext.sh
+++ Lesson3-4/ext.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-old_extension="$1"
-new_extension="$2"
-list=$(find . -type f -name "*.${old_extension}")
+read -p "Напишите исходное расширение " old_extension
+read -p "Напишите новое расширение " new_extension
+read -p "Напишите путь к файлам " destination
+list=$(find $destination -type f -name "*.${old_extension}")
 for file in $list; do
     mv "$file" "${file%.*}.$new_extension"
 done
